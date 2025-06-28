363b9524dba1c37e664f0c08ec3945962cd9e272
Sampreeti Atta <58343519+SampreetiAtta@users.noreply.github.com>
2025-06-29 00:14:07 +0300

    Create CalculatorTest.java

---
+++ Topic 4/CalculatorTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calc = new Calculator();
+        calc.add(5);
+        assertEquals(5, calc.getResult());
+    }
+
+    @Test
+    public void testSubtraction() {
+        Calculator calc = new Calculator();
+        calc.add(10);
+        calc.subtract(4);
+        assertEquals(6, calc.getResult());
+    }
+
+    @Test
+    public void testMultiplication() {
+        Calculator calc = new Calculator();
+        calc.add(3);
+        calc.multiply(4);
+        assertEquals(12, calc.getResult());
+    }
+
+    @Test
+    public void testExponentiation() {
+        Calculator calc = new Calculator();
+        calc.add(2);
+        calc.exponent(3); // 2^3 = 8
+        assertEquals(8, calc.getResult());
+    }
+
+    @Test
+    public void testReset() {
+        Calculator calc = new Calculator();
+        calc.add(10);
+        calc.reset();
+        assertEquals(0, calc.getResult());
+    }
+
+    @Test
+    public void testExponentZero() {
+        Calculator calc = new Calculator();
+        calc.add(5);
+        calc.exponent(0); // should be 1
+        assertEquals(1, calc.getResult());
+    }
+}
